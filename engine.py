03defdd937822fb755c9092ecf3165d77dbe60bf
Olly Mills <kinglslbob@gmail.com>
2019-06-20 22:27:33 +0100

    entity class added tutorial 2

--- engine.py
+++ engine.py
@@ -2,13 +2,16 @@ import tcod as libtcod
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+from entity import Entity
 from input_handlers import handle_keys
 def main():
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width/2)
-    player_y = int(screen_height/2)
+    player = Entity(int(screen_width/2), int(screen_height/2), '@', libtcod.white)
+    npc = Entity(int(screen_width/2 - 5), int(screen_height/2), '@', libtcod.yellow)
+    entities = [npc,player]
+
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GRAYSCALE|libtcod.FONT_LAYOUT_TCOD)
 
     libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
@@ -21,11 +24,15 @@ def main():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
         libtcod.console_set_default_foreground(con, libtcod.white)
-        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_put_char(con, player.x, player.y, '@', libtcod.BKGND_NONE)
         libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+
+        libtcod.console_set_default_foreground(0, libtcod.white)
+        libtcod.console_put_char(0, player.x, player.y, '@', libtcod.BKGND_NONE)
         libtcod.console_flush()
-        
-        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+
+        libtcod.console_put_char(con, player.x, player.y, ' ', libtcod.BKGND_NONE)
+        libtcod.console_put_char(0, player.x, player.y, ' ', libtcod.BKGND_NONE)
 
         action = handle_keys(key)
 
@@ -35,8 +42,7 @@ def main():
 
         if move:
             dx, dy = move
-            player_x += dx
-            player_y += dy
+            player.move(dx, dy)
 
         if exit:
             return True
