cc3aa819f312b07fb1f99d8770cb5a7e7d2ca08f
Olly Mills <kinglslbob@gmail.com>
2019-06-22 00:48:34 +0100

    tutorial 6 - now able to deal damage either way, nothing dies yet

--- entity.py
+++ entity.py
@@ -1,3 +1,4 @@
+import tcod as libtcod
 import math
 
 
@@ -35,10 +36,57 @@ class Entity:
                 get_blocking_entities_at_location(entities, self.x + dx, self.y + dy)):
             self.move(dx, dy)
 
+    def move_astar(self, target, entities, game_map):
+        # create a FOV map that has the dimensions of the map
+        fov = libtcod.map_new(game_map.width, game_map.height)
+
+        # Scan the current map each turn and set all the walls as unwalkable
+        for y1 in range(game_map.height):
+            for x1 in range(game_map.width):
+                libtcod.map_set_properties(fov, x1, y1, not game_map.tiles[x1][y1].block_sight,
+                                           not game_map.tiles[x1][y1].blocked)
+        # Scan all the objects to see if there are objects that must be navigated around
+        # Check also that the object isn't self or the target (so that the start and the end points are free)
+        # The AI class handles the situation if self is next to the target so it will not use this A* function anyway
+        for entity in entities:
+            if entity.blocks and entity != self and entity != target:
+                # set the tile as a wall so it must be navigated around
+                libtcod.map_set_properties(fov, entity.x, entity.y, True, False)
+
+        # Allocate a A* path
+        # The 1.41 is the normal diagonal cost of moving, it can be set as 0.0 if diagonal moves are prohibited
+        my_path = libtcod.path_new_using_map(fov, 1.41)
+
+        # compute the path between self's coordinates and the targets coordinates
+        libtcod.path_compute(my_path, self.x, self.y, target.x, target.y)
+
+        # Check if the path exists, and in this case, also the path is shorter than 25 tiles
+        # The path size matters if you want the monster to use alternative longer paths (for example through other rooms
+        # ) if for example the player is in a corridor
+        # It makes sense to keep path size relatively low to keep the monsters from running around the map if there's an
+        # alternative path really far away
+
+        if not libtcod.path_is_empty(my_path) and libtcod.path_size(my_path) < 25:
+            # Find the coordinates in the computed full path
+            x, y = libtcod.path_walk(my_path, True)
+            if x or y:
+                # Set self's coordinates to the next path tile
+                self.x = x
+                self.y = y
+        else:
+            # Keep the old move function as a backup so that if there are no paths (for example another monster
+            # blocks a corridor)
+            # it will still try to move towards the player (closer to the corridor opening)
+            self.move_towards(target.x, target.y, game_map, entities)
+        # delete the path to free memory
+        libtcod.path_delete(my_path)
+
+
     def distance_to(self, other):
         dx = other.x - self.x
         dy = other.y - self.y
         return math.sqrt(dx ** 2 + dy ** 2)
+
 
 def get_blocking_entities_at_location(entites, destination_x, destination_y):
     for entity in entites:
